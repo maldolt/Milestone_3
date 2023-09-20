4020d4aa09d78a457d7a930498bce4481fbaa242
Laura Velasco <maldolt@nv.ccsd.net>
2023-09-20 10:50:45 -0700

    fixed resource page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles/Navbar.scss';
 
-
 function Navbar() {
   return (
     <nav>
-      <ul>
+      
+      <ul className="navbar-right">
         <li>
           <Link to="/">Home</Link>
         </li>
+        <li>
+          <Link to="/resources">Resources</Link>
+        </li>
+      </ul>
+      <ul className="navbar-left">
         <li>
           <Link to="/login">Login</Link>
         </li>
         <li>
           <Link to="/signup">Sign Up</Link>
-        </li>
-        <li>
-          <Link to="/resources">Resources</Link>
         </li>
       </ul>
     </nav>
--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -6,16 +6,17 @@ const Resources = () => {
     <div className="resources-container">
       <h1>Resources</h1>
       <div className="resource-links">
+      <div className="resource">
+          <a href="https://www.getepic.com/" target="_blank" rel="noopener noreferrer">
+            <img src="https://play-lh.googleusercontent.com/4C7CwtFYnqg_nlr9djM7EH0uZHq2iqpad3K1LEBTD0471EuTqKIZZp-dPf3YmseaPN4" alt="Get Epic" />
+          </a>
+        </div>
         <div className="resource">
           <a href="https://storylineonline.net/" target="_blank" rel="noopener noreferrer">
             <img src="https://libapps-au.s3-ap-southeast-2.amazonaws.com/accounts/203715/images/Screen_Shot_2020-06-01_at_8.07.28_PM.png" alt="Story Online" />
           </a>
         </div>
-        <div className="resource">
-          <a href="https://www.getepic.com/" target="_blank" rel="noopener noreferrer">
-            <img src="https://i0.wp.com/westhill.wandsworth.sch.uk/wp-content/uploads/2021/01/get-epic.png?fit=225%2C225&ssl=1" alt="Get Epic" />
-          </a>
-        </div>
+
         <div className="resource">
           <a href="https://www.vooks.com/" target="_blank" rel="noopener noreferrer">
             <img src="https://images.crunchbase.com/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/bbnmzvhs4sxzp4vqxquq" alt="Resource 3" />
