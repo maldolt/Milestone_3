50e424bdd0f8ce0c4a561d31f4f9e751160c84e7
Laura Velasco <maldolt@nv.ccsd.net>
2023-09-23 15:37:28 -0700

    reordered URL to bottom

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,12 +4,14 @@ import "./styles/styles.scss";
 import { createClient } from "@supabase/supabase-js";
 import { useNavigate, Link } from "react-router-dom";
 
+//supabase
 const supabaseUrl = "https://qtzwzoszjisovyydpjww.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0end6b3N6amlzb3Z5eWRwand3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQyODc5NTksImV4cCI6MjAwOTg2Mzk1OX0.jVDzrA0WmZnpnK3x7T0Jno4siKt_vwcZrC2rwV01il8";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+//all dashboard functions
 const DashboardPage = () => {
   const [submittedData, setSubmittedData] = useState(null);
   const navigate = useNavigate();
--- src/components/NewBook.jsx
+++ src/components/NewBook.jsx
@@ -8,136 +8,135 @@ const supabaseUrl = "https://qtzwzoszjisovyydpjww.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0end6b3N6amlzb3Z5eWRwand3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQyODc5NTksImV4cCI6MjAwOTg2Mzk1OX0.jVDzrA0WmZnpnK3x7T0Jno4siKt_vwcZrC2rwV01il8";
 
-  const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-  const DashboardPage = () => {
-    const navigate = useNavigate();
-    const [title, setTitle] = useState("");
-    const [rating, setRating] = useState(0);
-    const [date, setDate] = useState(new Date());
-    const [image, setImage] = useState("");
-  
-    const handleTitleChange = (e) => {
-      setTitle(e.target.value);
-    };
-  
-    const handleDateChange = (e) => {
-      setDate(e.target.value);
-    };
+const NewBook = () => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [rating, setRating] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [image, setImage] = useState("");
 
-    const handleImageUrlChange = (e) => {
-      setImage(e.target.value);
-    };
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      console.log("handleSubmit called");
-      console.log("Title:", title);
-      console.log("Rating:", rating);
-      console.log("Date:", date);
-      console.log("Image:", image);
-  
-      try {
-        // Insert data into your Supabase table
-        const { data, error } = await supabase.from("Logs").insert([
-          {
-            title: title,
-            rating: rating,
-            date: date,
-            image_url: image,
-          },
-        ]);
-  
-        if (error) {
-          console.error("Error inserting data:", error);
-        } else {
-          console.log("Form submitted and data inserted:", data);
-  
-          // Redirect to the dashboard page after successful form submission
-          navigate("/dashboard");
-        }
-      } catch (error) {
-        console.error("Error:", error);
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleImageUrlChange = (e) => {
+    setImage(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("handleSubmit called");
+    console.log("Title:", title);
+    console.log("Rating:", rating);
+    console.log("Date:", date);
+    console.log("Image:", image);
+
+    try {
+      // Insert data into your Supabase table
+      const { data, error } = await supabase.from("Logs").insert([
+        {
+          title: title,
+          rating: rating,
+          date: date,
+          image_url: image,
+        },
+      ]);
+
+      if (error) {
+        console.error("Error inserting data:", error);
+      } else {
+        console.log("Form submitted and data inserted:", data);
+
+        // Redirect to the dashboard page after successful form submission
+        navigate("/dashboard");
       }
-    };
-  
-    return (
-      <div className="h-container">
-        <header>
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div className="h-container">
+      <header>
         <h1>📚Let's Add a New Book to Your Reading Log!📚</h1>
-        </header>
-        <section className="log-container">
-          <form onSubmit={handleSubmit}>
-            <div>
-              <div>
-                <label htmlFor="image">Image URL:</label>
-                <input
-                  type="url"
-                  id="image"
-                  accept="text"
-                  value={image}
-                  onChange={handleImageUrlChange}
-                />
-              </div>
-  
-              <label htmlFor="title">Title of Book:</label>
+      </header>
+      <section className="log-container">
+        <form onSubmit={handleSubmit}>
+          <div>
+           
+            <label htmlFor="title">Title of Book:</label>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={handleTitleChange}
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor="rating">Rating for Book:</label>
+            <StarRatings
+              rating={rating}
+              starRatedColor="gold"
+              changeRating={(newRating) => setRating(newRating)}
+              numberOfStars={5}
+              name="rating"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="date">Date:</label>
+            <input
+              type="date"
+              id="date"
+              value={date}
+              onChange={handleDateChange}
+              required
+            />
+          </div>
+          <div>
+              <label htmlFor="image">Image URL:</label>
               <input
-                type="text"
-                id="title"
-                value={title}
-                onChange={handleTitleChange}
-                required
+                type="url"
+                id="image"
+                accept="text"
+                value={image}
+                onChange={handleImageUrlChange}
               />
             </div>
-  
-            <div>
-              <label htmlFor="rating">Rating for Book:</label>
-              <StarRatings
-                rating={rating}
-                starRatedColor="gold"
-                changeRating={(newRating) => setRating(newRating)}
-                numberOfStars={5}
-                name="rating"
-              />
-            </div>
-  
-            <div>
-              <label htmlFor="date">Date:</label>
-              <input
-                type="date"
-                id="date"
-                value={date}
-                onChange={handleDateChange}
-                required
-              />
-            </div>
-            <br />
-            <button className="log-button" type="submit">
-              Submit
-            </button>
-          </form>
-          
-        </section>
-  <div style={{ textAlign: "center", marginTop: "20px" }}>
+            
+          <br />
+          <button className="log-button" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
         <Link to="/dashboard" className="log-button">
           Return to Dashboard
         </Link>
       </div>
-        <footer className="footer">
-        
-          <p>
-            &copy; 2023 Reading Log App |{" "}
-            <a
-              href={"https://github.com/maldolt/milestone_3.git"}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub Repository
-            </a>
-          </p>
-        </footer>
-      </div>
-    );
-  };
-  
-  export default DashboardPage;
+      <footer className="footer">
+        <p>
+          &copy; 2023 Reading Log App |{" "}
+          <a
+            href={"https://github.com/maldolt/milestone_3.git"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub Repository
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default NewBook;
